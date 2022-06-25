fc774dccbf4385c1b5a4b504e2934566ab9edb5e
Jean-Marie Marchand <47782394+JMarchandev@users.noreply.github.com>
2022-06-26 01:20:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,31 +41,3 @@ setInterval(async () => {
     currentlyRecording = false;
   }
 }, 5000);
-
-// const { launch, getStream } = require("puppeteer-stream");
-// const fs = require("fs");
-
-// const file = fs.createWriteStream(__dirname + "/test.webm");
-
-// async function test() {
-//   const browser = await launch({
-//     defaultViewport: {
-//       width: 1920,
-//       height: 1080,
-//     },
-//   });
-
-//   const page = await browser.newPage();
-//   await page.goto("http://localhost:3000");
-//   const stream = await getStream(page, { audio: true, video: true });
-//   console.log("recording");
-
-//   stream.pipe(file);
-//   setTimeout(async () => {
-//     await stream.destroy();
-//     file.close();
-//     console.log("finished");
-//   }, 1000 * 10);
-// }
-
-// test();
